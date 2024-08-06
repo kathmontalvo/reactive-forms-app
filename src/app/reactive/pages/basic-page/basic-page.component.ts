da4471212f9ad1997f0b7794866534ec2c249c46
kathm <kathmontalvo123@gmail.com>
2024-08-06 16:46:25 -0500

    validators service

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../../shared/service/validators.service';
 
 const rtx5090 = {
   name: 'RTX5090',
@@ -25,17 +26,17 @@ export class BasicPageComponent implements OnInit {
     inStorage: [0, [ Validators.required, Validators.min(0) ]],
   })
 
-  constructor( private fb: FormBuilder ){}
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  ){}
 
   ngOnInit(): void {
-
     this.myForm.reset(rtx5090)
-
   }
 
   isValidField(field: string):boolean | null {
-    return this.myForm.controls[field].errors
-    && this.myForm.controls[field].touched;
+    return this.validatorsService.isValidField(this.myForm, field)
   }
 
   getFieldError( field:string ): string | null {
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../../shared/service/validators.service';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -17,7 +18,10 @@ export class DynamicPageComponent {
 
   public newFavorite: FormControl = new FormControl('', Validators.required);
 
-  constructor( private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  ) {}
 
   get favoriteGames() {
     return this.myForm.get('favGames') as FormArray;
@@ -25,8 +29,7 @@ export class DynamicPageComponent {
 
 
   isValidField(field: string):boolean | null {
-    return this.myForm.controls[field].errors
-    && this.myForm.controls[field].touched;
+    return this.validatorsService.isValidField(this.myForm, field)
   }
 
   getFieldError( field:string ): string | null {
